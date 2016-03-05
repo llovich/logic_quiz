5011c578d2d170a3098db42687231e5c53de6d95
girllogic <lillianlovich@yahoo.com>
2016-03-04 23:26:56 -0600

    added temp nav buttons to use during dev

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def quiz
+  end
+
+  def result
+  end
+
+  def thanks
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get '/welcome' => 'pages#index'
+  get '/quiz' =>'pages#quiz'
+  get '/result' =>'pages#result'
+  get '/thanks' => 'pages#thanks'
+
   resources :logicians
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'logicians#index'
+   root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
